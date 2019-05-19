ce7891d95df2de1552cf1bd7ab0af5a3f9078154
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-05-19 17:39:38 +0300

    Add editor project

---
+++ TranslationToCode/LanguageInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TranslationToCode
+{
+    public class LanguageInfo
+    {
+        public string Name { get; set; }
+        public string Locale { get; set; }
+        public string LocalName { get; set; }
+    }
+}
